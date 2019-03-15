b795c69b6a57a8895753801cfe3d55c28aa08b27
Kye <kyerafferty2000@gmail.com>
2019-03-15 01:41:12 +0000

    Build for aoscp #3

--- aoscp_h850.mk
+++ aoscp_h850.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common aoscp stuff.
-$(call inherit-product, vendor/aoscp/config/Android.mk)
+$(call inherit-product, vendor/aoscp/config/common_full_phone.mk)
 
 # Inherit from h815 device
 $(call inherit-product, device/lge/h850/device.mk)
